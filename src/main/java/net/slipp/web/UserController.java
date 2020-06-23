9faa195b7c20d63650ee56098e5496562a9d983d
lee.younghyo <dldudgy7@gmail.com>
2020-06-23 16:45:49 +0900

    h2 database 설치 user 관련 url mapping 기능 html refactoring

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,30 +1,68 @@
 package net.slipp.web;
 
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
 
     private List<User> users = new ArrayList<>();
 
-    @PostMapping("/create")
+    @PostMapping("")
     public String create(User user) {
-        System.out.println(user);
-        users.add(user);
-        return "redirect:/list";
+        userRepository.save(user);
+        return "redirect:/users";
     }
 
-    @GetMapping("/list")
+    @GetMapping("")
     public String list(Model model) {
-        model.addAttribute("users", users);
+        model.addAttribute("users", userRepository.findAll());
         System.out.println(model.toString());
-        return "list";
+        return "/user/list";
+    }
+
+    @GetMapping("/form")
+    public String form() {
+        return "/user/form";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "/user/login";
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
