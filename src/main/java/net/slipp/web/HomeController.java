b6759cadc61da3a9cd03b8adbfa2fb4d0b09081d
lee.younghyo <dldudgy7@gmail.com>
2020-06-30 17:31:32 +0900

    index : 질문하기 리스트 표출

--- src/main/java/net/slipp/web/HomeController.java
+++ src/main/java/net/slipp/web/HomeController.java
@@ -1,15 +1,20 @@
 package net.slipp.web;
 
-import net.slipp.domain.User;
-import net.slipp.domain.UserRepository;
+import net.slipp.domain.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("questions", questionRepository.findAll());
         return "index";
     }
 }
